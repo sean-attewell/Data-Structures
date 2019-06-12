fa058d7ce864ec5333d18d086f3daff958a84ed8
sean-attewell <sean.attewell@gmail.com>
2019-06-12 16:04:23 +0100

    BST for_each passing tests

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -56,4 +56,8 @@ class BinarySearchTree:
             return self.value
 
     def for_each(self, cb):
-        pass
+        cb(self.value)
+        if self.left != None:
+            self.left.for_each(cb)
+        if self.right != None:
+            self.right.for_each(cb)
