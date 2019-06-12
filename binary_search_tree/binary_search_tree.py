a8f2f6a746eb51a8e9bf586d3a5a25bbad2108ad
sean-attewell <sean.attewell@gmail.com>
2019-06-12 14:39:29 +0100

    BST insert, contains and get_max passing tests

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,17 +1,59 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        # Check new node val < current nodes val
+        if value < self.value:
+            # if no left child place new node
+            if not self.left:
+                self.left = BinarySearchTree(value)
+            else:
+                # otherwise repeat process
+                self.left.insert(value)
+        # check new node val >= current nodes val
+        if value >= self.value:
+            # if no right child place new node
+            if not self.right:
+                self.right = BinarySearchTree(value)
+            # otherwise repeat process
+            else:
+                self.right.insert(value)
 
-  def contains(self, target):
-    pass
+    def contains(self, target):
+        # import pdb
+        # pdb.set_trace()
+        if self.value == target:
+            return True
+        if target < self.value:
+            if self.left:
+                return self.left.contains(target)
+            else:
+                return False
+        else:
+            if self.right:
+                return self.right.contains(target)
+            else:
+                return False
 
-  def get_max(self):
-    pass
+    # must be an implied False returned somewhere
+    # def contains(self, target):
+    #     if target == self.value:
+    #         return True
+    #     if target < self.value:
+    #         if self.left:
+    #             return self.left.contains(target)
+    #     else:
+    #         if self.right:
+    #             return self.right.contains(target)
 
-  def for_each(self, cb):
-    pass
+    def get_max(self):
+        if self.right != None:
+            return self.right.get_max()
+        else:
+            return self.value
+
+    def for_each(self, cb):
+        pass
--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,21 +1,35 @@
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        pass
 
-  def delete(self):
-    pass
+    def delete(self):
+        pass
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        pass
 
-  def get_size(self):
-    pass
+    def get_size(self):
+        pass
 
-  def _bubble_up(self, index):
-    pass
+    def _bubble_up(self, index):
+        # in worst case elem will need to make way to top of heap
+        while index > 0:
+            # get parent elem of this index
+            parent = (index - 1) // 2
+            # check if elem at index is higher priority than parent elem
+            if self.storage[index] > self.storage[parent]:
+                # if it is then swap them
+                self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
 
-  def _sift_down(self, index):
-    pass
+                # update index to be new spot that swapped elem now resides at
+                index = parent
+            else:
+                # otherwise, our elem is at a valid spot in the heap
+                # we no longer need to bubble up
+                break
+
+    def _sift_down(self, index):
+        pass
